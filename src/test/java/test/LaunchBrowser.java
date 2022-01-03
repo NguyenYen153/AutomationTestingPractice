959b11ede27dd65d1adbe52acd75aca4728eb3ed
Nguyen Yen <nguyenyenict1995@gmail.com>
2022-01-03 20:32:17 +0700

    Lession 14 and Lab

--- src/test/java/test/LaunchBrowser.java
+++ src/test/java/test/LaunchBrowser.java
@@ -1,16 +1,30 @@
 package test;
 
+
 import org.apache.commons.exec.OS;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LaunchBrowser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws  InterruptedException {
         //Location of Webdriver
         String chromeDriverLocation = null;
         String currentProjectLocation = System.getProperty("user.dir");
-        chromeDriverLocation = OS.isFamilyMac() ? currentProjectLocation.concat("/src/test/resource/drivers/chromedriver") :currentProjectLocation.concat("");
+        System.out.println(currentProjectLocation);
+        chromeDriverLocation = OS.isFamilyMac() ? currentProjectLocation.concat("/AutomationTestingPractice/src/test/resource/drivers/chromedriver")
+        :currentProjectLocation.concat("\\AutomationTestingPractice\\src\\test\\resources\\drivers\\chromedriver.exe");
 
+        // Chrome Browser option
+        ChromeOptions chromeoptions = new ChromeOptions();
+        chromeoptions.addArguments("--incognito");
 
         //Start browser session
+        System.setProperty("webdriver.chrome.driver", chromeDriverLocation);
         //Open a webpage
+        WebDriver driver = new ChromeDriver(chromeoptions);
+        driver.get("https://google.com");
+        Thread.sleep(3000);
+        driver.quit();
     }
 }
